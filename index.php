8e53aca82602a92b480a1c53fe9cd5ac329cd82d
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2021-02-07 16:48:07 +0700

    fix header

--- index.php
+++ index.php
@@ -1,13 +1,62 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Form Pendaftaran Anggota</title>
+    <!-- Load file CSS Bootstrap offline -->
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="index.css">
-    <title>Document</title>
 </head>
-<body>
+<body >
+<nav class="navbar navbar-expand-lg navbar-light">
+        <a class="navbar-brand" href="#">
+            <H1>bpjs</H1>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Beranda <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Profil</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Berita</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Jaminan Kesehatan</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Layanan</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Informasi publik</a>
+            </li>
+            </ul>
+        </div>
+    </nav> -->
     
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">Lindungi <span>keluargamu</span> , daftar <span> BPJS </span>  sekarang</h1>
+            <form action="daftar.php" method="POST">
+            <button type="submit" name="submit" class="btn btn-primary">DAFTAR </button>
+            </form>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row  testimoni1">
+            <div class="col">
+                <img src="assets/img/testi.jpg" class="img-fluid" alt="">
+            </div>
+            <div class="col">
+            <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae sed minima quidem neque iste itaque ut maxime, culpa velit dolores. Labore ipsa tempora, amet nulla at dolorum quia dicta. Possimus?</h3>
+            </div>
+        </div>
+    </div>
+
+
 </body>
 </html>
